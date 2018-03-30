81fa43ee1bb7b34f266fb3afab469997e9e95ef6
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:40 -0400

    Removed unnecessary header includes.

--- src/LavisController.h
+++ src/LavisController.h
@@ -7,21 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef LAVIS_CONTROLLER_H
 #define LAVIS_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
 
 #include <ModularClient.h>
 #include <PowerSwitchController.h>
